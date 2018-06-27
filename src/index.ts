164d34687b857383feb1510faece903a2bdf3d67
libang <libang@giga.build>
2018-06-27 13:32:34 +0800

    🎉 init yeah!

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { createServer } from 'http';
+import socket from 'socket.io';
+
+const server = createServer();
+
+const io = socket(server, {
+  // path: '/test',
+  // serveClient: false,
+  origins: '*:*',
+  // below are engine.IO options
+  // pingInterval: 10000,
+  // pingTimeout: 5000,
+  cookie: true
+});
+
+server.listen(3000, () => {
+  console.log('listen ', 3000);
+});
